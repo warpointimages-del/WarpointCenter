2b6c0f1e09f1da0a5cef5267b1c818041dce4de8
warpointimages-del <warpointimages@gmail.com>
2025-09-26 13:46:45 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,7 +9,9 @@ const serviceAccount = {
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs"
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: "googleapis.com"
 };
 
 admin.initializeApp({
@@ -18,6 +20,5 @@ admin.initializeApp({
 });
 
 const db = admin.database();
-const auth = admin.auth();
 
-module.exports = { db, auth };
+module.exports = { admin, db };
